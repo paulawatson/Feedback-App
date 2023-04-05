a8444652ddf6bf6b8157254882a843fa9435590d
Paula Watkins <100526323+paulawatson@users.noreply.github.com>
2023-04-05 16:49:57 +0100

    Add files via upload

---
+++ component/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header() {
+    return (
+        
+            <header>
+               <div className ="container">
+                <h2>Feedback UI</h2>
+               </div>
+            </header>
+    
+    )
+}
+
+export default Header
